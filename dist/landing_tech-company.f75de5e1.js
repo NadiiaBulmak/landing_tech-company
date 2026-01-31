461e55763f40becad9c35eafe4f86bc814b7e5a2
Nadiia Bulmak <nbulmak@gmail.com>
2026-01-31 13:53:51 +0200

    add build for live demo

---
+++ dist/landing_tech-company.f75de5e1.js
@@ -0,0 +1,27 @@
+'use strict';
+const form = document.getElementById('form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    form.reset();
+});
+const menuToggle = document.querySelector('.icon--menu');
+const closeButton = document.querySelector('.icon--close');
+const body = document.body;
+const menu = document.getElementById('menu');
+const menuLinks = document.querySelectorAll('.menu__nav a');
+menuToggle.addEventListener('click', function() {
+    body.classList.add('no-scroll');
+    menu.classList.add('open');
+});
+closeButton.addEventListener('click', function() {
+    body.classList.remove('no-scroll');
+    menu.classList.remove('open');
+});
+menuLinks.forEach((link)=>{
+    link.addEventListener('click', function() {
+        body.classList.remove('no-scroll');
+        menu.classList.remove('open');
+    });
+});
+
+//# sourceMappingURL=landing_tech-company.f75de5e1.js.map
